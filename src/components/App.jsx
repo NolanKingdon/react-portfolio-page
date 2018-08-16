853ad63acf2e3c1590eb0ba54309e61d23d31380
Nolan Kingdon <nolan.kingdon@powerschool.com>
2018-08-16 16:53:02 -0400

    Beginning to add in structure and first components

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NavMain from './Nav-Bar/navMain.jsx';
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <NavMain className = "nav-bar test"/>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/Nav-Bar/navMain.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/nav-bar.css';
+
+class NavMain extends Component {
+
+  render(){
+    return(
+      <div className = "nav-bar">
+        <h1>NAVBAR</h1>
+      </div>
+    )
+  }
+}
+
+export default NavMain;
