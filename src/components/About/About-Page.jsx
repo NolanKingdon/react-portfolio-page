2b2ad35b6f73ca132d6884f0c73753c3a96263a9
Nolan Kingdon <nolan.kingdon@powerschool.com>
2018-08-20 16:58:47 -0400

    Added extra folders

---
+++ src/components/About/About-Page.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './css/about.css';
+
+class AboutPage extends Component {
+  render(){
+    return(
+      <div className = "about" style = {{textAlign: "center"}}>
+        <div className = "about-head">
+          <hr className = "about-hr" />
+          <h2 className = "about-left">About</h2>
+          <hr className = "about-hr" />
+          <h1>Nolan Kingdon</h1>
+          <h2>Project Manager</h2>
+          <h3><a href = "tel:9052201705">(905)-220-1705</a></h3>
+          <h3><a href = "mailto:nolan.kingdon@gmail.com">nolan.kingdon@gmail.com</a></h3>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AboutPage;
