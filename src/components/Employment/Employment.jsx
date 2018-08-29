228acd7b1d87e4efa837f70ced06a5a8cd8f4d33
Nolan Kingdon <nolan.kingdon@gmail.com>
2018-08-29 16:59:34 -0400

    Added stylings for the Employment items

--- src/components/Employment/Employment.jsx
+++ src/components/Employment/Employment.jsx
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import EmploymentItem from './Employment-Item.jsx';
+import './css/Employment.css';
+import PSLogo from './images/PS-logo.png'
+import BVPLogo from './images/BVP-logo.png'
 
 class Employment extends Component {
 
   render(){
     return(
       <div style = {{position: "absolute", left: 500}}>
-        Test
+        {/*Consider making this a cool Grid/Tile fade in to reduce whitespace*/}
+        {/*Turn this into a map function with a functional backend using either firebase or a PHP DB depending on where you host this*/}
+        <EmploymentItem
+          Company = "Buona Vita Pizza"
+          Title = "Web Developer"
+          Time = "May - Present"
+          Description = "Web Developer for Buona Vita Pizza. Made a website. More placeHolder Text."
+          image = {BVPLogo}
+        />
+      <EmploymentItem
+        Company = "PowerSchool"
+        Title = "Project Manager"
+        Time = "January 2018 - Present"
+        Description = "Project Manager for the Registration department in PowerSchool. Managed multiple implementation projects of PowerSchool's Online registration software."
+        image = {PSLogo}
+      />
+      <EmploymentItem
+        Company = "PowerSchool"
+        Title = "Online Solution Specialist"
+        Time = "June 2017 - January 2018"
+        Description = "Online Solution Specialist for the Registration department in PowerSchool. Designed and deployed online registration forms using a custom CMS."
+        image = {PSLogo}
+      />
+      {/*End Potential Map Function*/}
       </div>
     )
   }
