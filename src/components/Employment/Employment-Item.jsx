82447c2fb53ffb28be64a59cfc514d607db32be4
Nolan Kingdon <nolan.kingdon@gmail.com>
2018-08-30 11:20:33 -0400

    Added flex box and updated assets

--- src/components/Employment/Employment-Item.jsx
+++ src/components/Employment/Employment-Item.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
+import './css/Employment.css';
 
 class EmploymentItem extends Component {
 
   render(){
     return(
-      <div className = "employment-container">
+      <div className = "employment-item-container">
         <img className = "employer-logo" src = {this.props.image}></img>
         <div>
-          <h1 className = "employment-header">{this.props.Company}</h1>
-          <h2 className = "employment-header">{this.props.Title}</h2>
-          <p className = "employment-time">{this.props.Time}</p>
+          <h1 className = "employment-header" id = "employment-h1">{this.props.Company}</h1>
+          <h2 className = "employment-header" id = "employment-h2">{this.props.Title}</h2>
+          <p className = "employment-time" id = "employment-time">{this.props.Time}</p>
           <hr className = "employment-hr"/>
-          <p className = "employment-description">{this.props.Description}</p>
+          <p className = "employment-description" id = "employment-p">{this.props.Description}</p>
         </div>
       </div>
     )
--- src/components/Employment/Employment.jsx
+++ src/components/Employment/Employment.jsx
@@ -8,28 +8,27 @@ class Employment extends Component {
 
   render(){
     return(
-      <div style = {{position: "absolute", left: 500}}>
-        {/*Consider making this a cool Grid/Tile fade in to reduce whitespace*/}
-        {/*Turn this into a map function with a functional backend using either firebase or a PHP DB depending on where you host this*/}
+      <div className = "employment-container" style = {{position: "absolute", left: 300}}>
+        {/*This is now flex box. Not sure if I like that. It feels kind of lame with mobile.*/}
         <EmploymentItem
           Company = "Buona Vita Pizza"
           Title = "Web Developer"
-          Time = "May - Present"
-          Description = "Web Developer for Buona Vita Pizza. Made a website. More placeHolder Text."
+          Time = "May 2018 - Present"
+          Description = "Web Developer for Buona Vita Pizza. Worked with the owners to create a simple webpage to hold their online menu. Used a wireframe to create the website from scratch, as well as hosted on a third party hosting service. Currently working to develop a PHP based online ordering system, and a PHP backend for easy Price updates. Currently responsible for maintenance of content."
           image = {BVPLogo}
         />
       <EmploymentItem
         Company = "PowerSchool"
         Title = "Project Manager"
         Time = "January 2018 - Present"
-        Description = "Project Manager for the Registration department in PowerSchool. Managed multiple implementation projects of PowerSchool's Online registration software."
+        Description = "Project Manager for the Registration department in PowerSchool. Managed multiple implementation projects of PowerSchool's Online registration software, often liasing with clients in order to maintain the timeline and drive projects forward. Responsibilities also include running and reporting QA tasks for forms before launch, in which I made a Python based reporting tool to optimize reporting - Saving roughly an hour of time in reporting a week."
         image = {PSLogo}
       />
       <EmploymentItem
         Company = "PowerSchool"
         Title = "Online Solution Specialist"
         Time = "June 2017 - January 2018"
-        Description = "Online Solution Specialist for the Registration department in PowerSchool. Designed and deployed online registration forms using a custom CMS."
+        Description = "Online Solution Specialist for the Registration department in PowerSchool. Designed and deployed online registration forms using HTML, CSS, and JavaScript through a custom CMS. Responsible for both front and backend configuration. Back end responsibilities included leveraging the CMS and API connections to ensure secure data delivery."
         image = {PSLogo}
       />
       {/*End Potential Map Function*/}
