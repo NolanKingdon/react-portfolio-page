61d22b27ed7ee1a553b1b28ac8a91ab55ddffd30
Nolan Kingdon <nolan.kingdon@gmail.com>
2018-09-05 21:34:36 -0400

    Added navbar show/hide functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { CSSTransitionGroup } from 'react-transition-group';
 import NavMain from './Nav-Bar/navMain.jsx';
 import AboutPage from './About/About-Page.jsx';
 import Header from './Header-Bar/Header.jsx';
 import Employment from './Employment/Employment.jsx';
 import Education from './Education/Education.jsx';
-import './css/transitions.css';
+import Animate from 'react-move/Animate';
+import { easeExpOut } from 'd3-ease';
 
 class App extends Component {
 //Commented out code was an attempt at having the navBar display, and then move to -20vw to improve UI
@@ -14,33 +14,11 @@ class App extends Component {
     super(props);
     this.state = {
       page: "Education",
-      navOrigin: "0vw"
+      navHidden: false
     }
-    //this.navUpdate = this.navUpdate.bind(this);
-    //this.mouseEntered = this.mouseEntered.bind(this);
     this.pageChanger = this.pageChanger.bind(this);
+    this.displayToggle = this.displayToggle.bind(this);
   }
-
-  // navUpdate(){
-  //   this.setState({
-  //     navOrigin: "-20vw"
-  //   })
-  //   console.log("Moved: ", this.state.navOrigin);
-  // }
-  //
-  // mouseEntered(){
-  //   console.log("Mouse entered");
-  //   this.setState({
-  //     navOrigin: "0vw"
-  //   })
-  // }
-  //
-  // mouseLeft(){
-  //   console.log("Mouse Leaving")
-  //   this.setState({
-  //     navOrigin: -"20vw"
-  //   })
-  // }
 
   pageChanger(newPage){
     console.log("Page Changer = ", newPage);
@@ -49,34 +27,54 @@ class App extends Component {
     });
   }
 
+  displayToggle(){
+    this.setState({navHidden: !this.state.navHidden});
+  }
+
   render(){
     return(
       <div>
-        <NavMain
-          className = "nav-bar test"
-          changer = {this.pageChanger}
-        />
-      {/*
-        navUpdate = {this.navUpdate}
-        navOrigin = {this.state.navOrigin}
-        onMouseEnter = {this.mouseEntered}
-        onMouseLeave = {this.mouseLeft}
-        */}
+        <Animate
+          start={()=> ({
+            x: "0vw",
+          })}
+
+          update={() => ({
+            x: [ this.state.navHidden ? "-20vw" : "0vw"],
+            timing: { duration: 1000, ease: easeExpOut },
+          })}
+        >
+          {(state) => {
+            const { x } = state;
+
+            return(
+              <NavMain
+                className = "nav-bar test"
+                changer = {this.pageChanger}
+                displayToggle = {this.displayToggle}
+                navHidden = {this.state.navHidden}
+                divStyle = {{
+                  position: "fixed",
+                  top: "0",
+                  left: x,
+                  width: "22vw",
+                  height: "100%",
+                  backgroundColor: "#023670",
+                  color: "white",
+                  textAlign: "left",
+                  mozBoxShadow:    "4px 3px 10px 1px #ccc",
+                  webkitBoxShadow: "4px 3px 10px 1px #ccc",
+                  boxShadow:       "4px 5px 10px 1px gray",
+                  WebkitTransform: `translate3d(${x}px, 0, 0)`,
+                  transform: `translate3d(${x}px, 0, 0)`,
+                }}
+              />
+            )
+          }}
+        </Animate>
         <Header
           page = {this.state.page}
         />
-      {/*Add the CSS file here so we can assign the fades*/}
-      {/*          <CSSTransitionGroup
-                  transitionName = "main-pages"
-                  transitionEnterTimeout = {500}
-                  transitionLeaveTimeout = {300}
-                >
-
-                </CSSTransitionGroup>
-
-                */}
-
-
         {this.state.page == "About" && <AboutPage />}
         {this.state.page == "Employment History" && <Employment />}
         {this.state.page == "Education" && <Education />}
