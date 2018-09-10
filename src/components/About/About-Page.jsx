e239f45aaeb3aad087a12dd6214a0e41015c7593
Nolan Kingdon <nolan.kingdon@gmail.com>
2018-09-10 15:29:06 -0400

    Fleshing out the About page

--- src/components/About/About-Page.jsx
+++ src/components/About/About-Page.jsx
@@ -8,6 +8,12 @@ class AboutPage extends Component {
       <div className = "about" style = {{textAlign: "center"}}>
         <div className = "about-head">
           <img src = {selfImage} className = "self-image"/>
+          <div className = "about-text">
+            <h1 id = "about-hello">Hello.</h1>
+            <p id = "about-intro">My name is Nolan - I am a web developer.</p>
+            <p id = "about-background">I currently work at PowerSchool as a Project Manager.</p>
+            <p id = "about-invitation">Feel free to explore around my site.</p>
+          </div>
         </div>
       </div>
     )
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,7 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      page: "Education",
+      page: "About",
       navHidden: false,
       showInfoSlider: false,
       currentCourse: "None"
