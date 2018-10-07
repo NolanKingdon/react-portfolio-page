603045ded4829b6a7c577545da30bc634424eea8
Nolan Kingdon <nolan.kingdon@gmail.com>
2018-10-06 23:09:58 -0400

    Added in Mobile Stylings

--- src/components/Header-Bar/Header.jsx
+++ src/components/Header-Bar/Header.jsx
@@ -5,7 +5,7 @@ class Header extends Component {
 
   render(){
     return(
-      <div>
+      <div style = {this.props.dynamicTopMargin}>
         <hr className = "about-hr" />
         <h2 className = "about-left">{this.props.page}</h2>
         <hr className = "about-hr" />
