05153825b163c9e54a442b1803670013f0ae60c9
Nolan Kingdon <nolan.kingdon@gmail.com>
2019-08-27 16:22:44 -0400

    Updated employment history

--- src/components/Employment/Employment.jsx
+++ src/components/Employment/Employment.jsx
@@ -32,22 +32,22 @@ class Employment extends Component {
               <EmploymentItem
                 Company = "PowerSchool"
                 Title = "Implementation Specialist"
-                Time = "January 2018 - Present"
-                Description = "Work hands on with multiple projects using HTML, CSS, Javascript, and C# through PowerSchool's custom CMS to create mobile friendly school registration forms for first year clients. Configured secure data transfer using API connections to client Databases"
+                Time = "January 2018 - December 2018"
+                Description = "Work hands on with multiple projects using HTML, CSS, and Javascript through PowerSchool's custom CMS to create mobile friendly school registration forms for first year clients. Responsible for project implementations for districts containing up to 25,000 students."
                 image = {PSLogo}
                 />
               <EmploymentItem
                 Company = "Buona Vita Pizza"
                 Title = "Web Developer"
-                Time = "May 2018 - Present"
-                Description = "Worked with the owners to create a simple webpage to hold their online menu. Used a wireframe to create the website from scratch, as well as hosted on a third party hosting service. Currently working to develop a PHP based online ordering system, and a PHP backend for easy price updates. Currently responsible for maintenance of content."
+                Time = "May 2018 - December 2018"
+                Description = "Worked with the owners to create a simple webpage to hold their online menu. Used a wireframe to create the website from scratch, as well as hosted on a third party hosting service. Currently working to develop a React based online ordering system, and a PHP backend for easy price updates. Currently responsible for maintenance of content."
                 image = {BVPLogo}
                 />
               <EmploymentItem
                 Company = "PowerSchool"
                 Title = "Online Solution Specialist"
                 Time = "June 2017 - January 2018"
-                Description = "Online Solution Specialist for the Registration department in PowerSchool. Designed and deployed online registration forms using HTML, CSS, and JavaScript through a custom CMS. Responsible for both front and backend configuration. Back end responsibilities included leveraging the CMS and API connections to ensure secure data delivery."
+                Description = "Online Solution Specialist for the Registration department in PowerSchool. Maintained existing online registration forms using HTML, CSS, and JavaScript through a custom CMS. Responsibilities included ensuring secure data delivery once collected. Responsible for maintaining forms for school districts of up to 7,000 students."
                 image = {PSLogo}
                 />
             </div>
