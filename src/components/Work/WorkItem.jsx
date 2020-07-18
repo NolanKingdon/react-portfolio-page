26ac2d11484db621b150a137761a5cb287e45016
Nolan Kingdon <nolan.kingdon@gmail.com>
2020-07-18 14:37:29 -0400

    Updated CSS and removed mobile versions

--- src/components/Work/WorkItem.jsx
+++ src/components/Work/WorkItem.jsx
@@ -8,17 +8,15 @@ class WorkItem extends Component {
       this.props.projectSelector(this.props.title);
     }
 
-
     render(){
-      {/* Instead of linking out, maybe provide a quick description of what it actually is. Have a Div element slide on in from the side */}
       return(
-        <button className = "education-item-button" onClick = {() => this.handleClick()}>
-          <div className = "education-item-grid">
+        <button className = "work-item-button" onClick = {() => this.handleClick()}>
+          <div className = "work-item-grid">
             <img src = {this.props.image} className = "work-logo" alt = "Logo"/>
             <div>
-              <h2 className = "education-item-title">{this.props.title}</h2>
+              <h2 className = "work-item-title">{this.props.title}</h2>
               <hr className = "work-item-hr"/>
-              <p className = "education-item-note-text">{this.props.noteText}</p>
+              <p className = "work-item-note-text">{this.props.noteText}</p>
             </div>
           </div>
         </button>
