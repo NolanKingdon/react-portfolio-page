39b716830c7597f12b6bc1c2a0948d97161f5499
Nolan Kingdon <nolan.kingdon@gmail.com>
2020-08-03 16:06:30 -0400

    Fixing mobile css - better alignment for employment items.

--- src/components/Employment/Employment-Item.jsx
+++ src/components/Employment/Employment-Item.jsx
@@ -8,11 +8,11 @@ class EmploymentItem extends Component {
       <div className = "employment-item-container">
         <img className = "employer-logo" src = {this.props.image} alt = "logo"></img>
         <div>
-          <h1 className = "employment-header" id = "employment-h1">{this.props.Company}</h1>
-          <h2 className = "employment-header" id = "employment-h2">{this.props.Title}</h2>
-          <p className = "employment-time" id = "employment-time">{this.props.Time}</p>
+          <h1 className = "employment-header employment-h1">{this.props.Company}</h1>
+          <h2 className = "employment-header employment-h2">{this.props.Title}</h2>
+          <p className = "employment-time">{this.props.Time}</p>
           <hr className = "employment-hr"/>
-          <p className = "employment-description" id = "employment-p">{this.props.Description}</p>
+          <p className = "employment-description">{this.props.Description}</p>
         </div>
       </div>
     )
