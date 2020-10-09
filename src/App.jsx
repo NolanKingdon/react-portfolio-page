c4def891b6b68dbb32e647222a1f3efe8e07e54f
Nolan Kingdon <nolan.kingdon@gmail.com>
2020-10-08 21:29:07 -0400

    converted json files to jsx for better image support, potential future DOM markup support

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,44 @@ import React, { Component } from 'react';
 import Page from './components/Page';
 import NavigationBar from './components/NavigationBar';
 import './components/css/styles.css';
-import navigationItems from './model/navigation.json';
-import pageContent from './model/pageContent.json'
+import navigationItems from './model/navigation.jsx';
+import pageContent from './model/pageContent.jsx';
 
 class App extends Component {
   constructor(props){
     super(props);
 
     this.state = {
-      currentPage: "My Work"
+      currentPage: "About",
+      navExtended: true
     }
 
     this.changeState = this.changeState.bind(this);
+    this.toggleNavbar = this.toggleNavbar.bind(this);
   }
 
   changeState(page){
     this.setState({currentPage: page});
   }
 
+  toggleNavbar(){
+    let extended = this.state.navExtended;
+    this.setState({navExtended: !extended});
+  }
+
   // Rendering our components.
   render(){
+
     return (
       <div className="main-flex-panel">
-        <NavigationBar items={navigationItems} changeState={this.changeState}></NavigationBar>
-        <Page currentPage={this.state.currentPage} pageContents={pageContent[this.state.currentPage]}></Page>
+        <NavigationBar
+          items={navigationItems}
+          changeState={this.changeState} 
+          toggleHandler={this.toggleNavbar}
+          extended={this.state.navExtended}/>
+        <Page
+          currentPage={this.state.currentPage}
+          pageContents={pageContent[this.state.currentPage]} />
       </div>
     );
   }
---
+++ src/model/navigation.jsx
@@ -0,0 +1,25 @@
+import aboutIcon from '../images/navbar/nav-icons/white/nav-icon-person-white.png';
+import educationIcon from '../images/navbar/nav-icons/white/nav-icon-grad-cap-white.png';
+import workIcon from '../images/navbar/nav-icons/white/nav-icon-code-white.png';
+import employmentIcon from '../images/navbar/nav-icons/white/nav-icon-briefcase-white.png';
+import contactIcon from '../images/navbar/nav-icons/white/nav-icon-envelope-white.png';
+
+let navigation = {
+    "About": {
+        "icon": aboutIcon
+    },
+    "Education": {
+        "icon": educationIcon
+    },
+    "My Work": {
+        "icon": workIcon
+    },
+    "Employment History": {
+        "icon": employmentIcon
+    },
+    "Contact": {
+        "icon": contactIcon
+    }
+};
+
+export default navigation;
